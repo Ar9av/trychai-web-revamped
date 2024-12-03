3b8970a0a2484b11c4c6d59fece88ef6b5bb2614
Ar9av <f2016731@pilani.bits-pilani.ac.in>
2024-12-03 11:49:38 +0530

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,8 +30,6 @@ export default function RootLayout({
         <head>
           <meta property="og:image" content="/favicon.ico" />
           <link rel="icon" href="/favicon.ico" type="image/x-icon" sizes="any" />
-          <link rel="icon" href="/favicon-32x32.png" type="image/png" sizes="32x32" />
-          <link rel="icon" href="/favicon-16x16.png" type="image/png" sizes="16x16" />
         </head>
         <body className={inter.className}>
         <ClerkProvider>
